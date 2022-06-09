d29b0d93eb4f689b1f2e69e53876da84488270d3
Kim Bob <kim+git@chno.de>
2022-06-09 03:31:43 +0200

    foo

--- displayRun.py
+++ displayRun.py
@@ -164,9 +164,7 @@ def render_content(draw_blk: TImageDraw, image_blk: TImage,  draw_red: TImageDra
     s.sendall(b'get battery')
     data = s.recv(1024)
     s.close()
-    #battery = int(repr(data).replace('singlebattery: ', '').replace('battery: ', '').rstrip('\n'))
-    battery = re.findall(r'\d+', repr(data))
-    logger.info("FOO" + join(battery))
+    battery = int(''.join(re.findall(r'\d+', repr(data))))
 
     # Month-Tally-Overview
     current_height += PADDING_TOP
